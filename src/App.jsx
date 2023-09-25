0cf781a3f31ef256efc7050896bb97a1a3317b76
AristideI <a.isingizwe@alustudent.com>
2023-09-25 16:10:19 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="font-bold text-5xl">The saint</h1>;
+  return <h1 className="font-bold text-5xl">The saint and the rest</h1>;
 }
 
 export default App;
